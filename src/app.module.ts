4f0530914f9607578773c2d833ddd8c3d1211933
caleb <calebchoi@genfitx.com>
2024-05-26 16:40:04 +0900

    config 설정 nestjs 버전

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
 
+const NODE_ENV = process.env.NODE_ENV;
+const envFilePath = ".env" + (NODE_ENV == "local" ? ".local" : NODE_ENV == "development" ? ".development" : "");
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ envFilePath })],
   controllers: [AppController],
   providers: [AppService],
 })
