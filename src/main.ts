065f80a5ca05ec064a44681176f12f5986792e81
caleb <calebchoi@genfitx.com>
2024-05-27 14:44:01 +0900

    nestjs winston 로그 설정

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { middleware } from "./app.middleware";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import { winstonLogger } from "./logger/winston.utils";
 
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), { logger: winstonLogger });
 
   middleware(app);
   //https://stackoverflow.com/questions/14043926/node-js-connect-only-works-on-localhost
