4fe5f01ff369a1edd0fa604dbb0d911148ff9e51
caleb <calebchoi@genfitx.com>
2024-05-27 13:48:59 +0900

    파일 업로드 다운로드 local & S3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,25 +1,40 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { UploadedFile, UploadedFiles } from "./decorators";
 import { FastifyFile } from "./types/fastify.file";
+import * as fs from "fs";
+import { join } from "path";
+import { AwsService } from "./aws/aws.service";
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly awsService: AwsService
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
   @Post()
-  async fileUploadTest(
-    @UploadedFile("profile") profile: FastifyFile,
-    @Body("name") name: string,
-    @UploadedFiles("images") images: FastifyFile[]
-  ): Promise<string> {
-    console.log("profile :", profile);
+  async fileUploadTest(@UploadedFile("profile") profile: FastifyFile, @Body("name") name: string, @UploadedFiles("images") images: FastifyFile[]) {
+    //로컬에 저장하기
+    const path = join(process.cwd(), "public", "assets", "images", profile.filename);
+    fs.writeFileSync(path, profile.value);
+    //S3업로드 하기
     console.log("name :", name);
     console.log("images :", images);
-
-    return "body";
+    await this.awsService.putOubjects(images);
+    return { is_success: true };
+  }
+  @Get("file")
+  async getFileByS3(@Query("key") Key: string) {
+    const url = await this.awsService.getPresignedURL(Key);
+    return { url };
+  }
+  @Get("local")
+  async getFileByLocal(@Query("key") Key: string) {
+    const path = "public/assets/";
+    return path + Key;
   }
 }
